699f1863f79672123a16b84edd0fdd1901428fb2
BrianLamWC <73526157+BrianLamWC@users.noreply.github.com>
2022-03-27 17:57:34 +0900

    Update Navigation.js

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -4,7 +4,7 @@ const Navigation = ({onRouteChange, isSignedIn}) => {
     if (isSignedIn) { 
             return(
             <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-                <p onClick={() => onRouteChange('SignOut')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+                <p onClick={() => onRouteChange('SignOut')} className='f3 link dim black underline pa3 pointer'>Sign Out</p> //
             </nav>
         );
     } else {
@@ -18,3 +18,5 @@ const Navigation = ({onRouteChange, isSignedIn}) => {
 }
 
 export default Navigation;
+
+//Displays either 'Sign Out' if user is signed in and 'Sign in/Register' if user is not at the top right corner.
